8d56926de732fac52f55a3d6ec5e84e4d258cf16
ClementCadieux <cadieuxclement@gmail.com>
2024-12-05 08:11:25 -0500

    Day 5.2: invalidLines

--- 2024/Day5/main2.py
+++ 2024/Day5/main2.py
@@ -3,4 +3,6 @@ import main1 as base
 if __name__ == "__main__":
     rules, prints = base.readFile("2024\\Day5\\test.txt")
 
+    invalidLines = [line for line in prints if not base.validatePrint(line, rules)]
+
     
