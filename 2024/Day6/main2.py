7fca4a9268191fb8ef48d68affb202e6d688c126
ClementCadieux <cadieuxclement@gmail.com>
2024-12-06 15:13:09 -0500

    getTotal()

--- 2024/Day6/main2.py
+++ 2024/Day6/main2.py
@@ -19,6 +19,47 @@ def getDirToLook(lines, corner):
 
     return dirToLook
 
+def getTotal(getDirToLook, lines, corners):
+    total = 0
+
+    for corner in corners:
+        dirToLook = getDirToLook(lines, corner)
+
+        i,j = corner
+
+        match dirToLook:
+            case ">":
+                if i != 0:
+                    for idx in range(j, -1, -1):
+                        if lines[i][idx] == "^" and lines[i - 1][idx] != "#":
+                            total += 1
+                        elif lines[i][idx] == "#":
+                            break
+            
+            case "^":
+                if j != 0:
+                    for idx in range(i, len(lines)):
+                        if lines[idx][j] == "<" and lines[idx][j - 1] != "#":
+                            total += 1
+                        elif lines[idx][j] == "#":
+                            break
+
+            case "<":
+                if i != len(lines) - 1:
+                    for idx in range(j, len(lines[i])):
+                        if lines[i][idx] == "v" and lines[i + 1][idx] != "#":
+                            total += 1
+                        elif lines[i][idx] == "#":
+                            break
+
+            case "v":
+                if j != len(lines[i]) - 1:
+                    for idx in range(i, -1, -1):
+                        if lines[idx][j] == ">" and lines[idx][j + 1] != "#":
+                            total += 1
+                        elif lines[idx][j] == "#":
+                            break
+
 if __name__ == "__main__":
     lines = base.readFile("2024\\Day6\\test.txt")
 
@@ -28,8 +69,8 @@ if __name__ == "__main__":
 
     corners = base.corners
 
-    for corner in corners:
-        dirToLook = getDirToLook(lines, corner)
+    total = getTotal(getDirToLook, lines, corners)
+    
 
 
 
