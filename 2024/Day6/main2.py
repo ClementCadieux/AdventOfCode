492444f6a4b880afd9ac943bab2267560aaa224b
ClementCadieux <cadieuxclement@gmail.com>
2024-12-06 15:03:43 -0500

    getDirToLook()

--- 2024/Day6/main2.py
+++ 2024/Day6/main2.py
@@ -1,5 +1,17 @@
 import main1 as base
 
+def getDirToLook(lines, corner):
+    i,j = corner
+
+    dirAtCorner = lines[i][j]
+
+    iBef = i - 1 if dirAtCorner == "<" else i + 1 if dirAtCorner == ">" else i
+    jBef = j - 1 if dirAtCorner == "v" else j + 1 if dirAtCorner == "^" else j
+
+    dirToLook = lines[iBef][jBef]
+
+    return dirToLook
+
 if __name__ == "__main__":
     lines = base.readFile("2024\\Day6\\test.txt")
 
@@ -8,6 +20,9 @@ if __name__ == "__main__":
     lines = base.processGuard(lines, i, j)
 
     corners = base.corners
+
+    for corner in corners:
+        getDirToLook(lines, corner)
 
     
 
