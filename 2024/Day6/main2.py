c3bb5bef0c69214cc61f1c1a1c98b70eee7ef6ac
ClementCadieux <cadieuxclement@gmail.com>
2024-12-06 19:26:39 -0500

    I'm goated?

--- 2024/Day6/main2.py
+++ 2024/Day6/main2.py
@@ -2,17 +2,11 @@ import main1 as base
 import copy
 
 total = set()
-firstTurn = False
-firstLine = []
 
-def processGuard(lines, i, j, dir, obstacle, tilesByDir):
-    global firstTurn
-    global firstLine
+def processGuard(lines, i, j, dir, obstacle, tilesByDir, seen):
     inMap = True
 
     while inMap:
-        if not firstTurn:
-            firstLine.append((i,j))
 
         changed = True
 
@@ -20,7 +14,6 @@ def processGuard(lines, i, j, dir, obstacle, tilesByDir):
             inMap, newDir = base.getDir(lines, inMap, i, j, dir)
             if dir != newDir:
                 dir = newDir
-                firstTurn = True
             else:
                 changed = False      
 
@@ -32,9 +25,10 @@ def processGuard(lines, i, j, dir, obstacle, tilesByDir):
         lines[i] = lines[i][:j] + dirChar + lines[i][j + 1:]
         
         tilesByDir[dir].append((i, j))
+        seen.add((i,j))
 
-        if not obstacle and firstTurn:
-            obstacleInFront(lines, i, j, dir, tilesByDir) 
+        if not obstacle:
+            obstacleInFront(lines, i, j, dir, tilesByDir, seen) 
 
         if inMap:
             match dir:
@@ -49,11 +43,11 @@ def processGuard(lines, i, j, dir, obstacle, tilesByDir):
 
     return lines, False
         
-def obstacleInFront(lines, i, j, dir, tilesByDir):
+def obstacleInFront(lines, i, j, dir, tilesByDir, seen):
     global total
-    global firstLine
     linesCopy = copy.deepcopy(lines)
     tilesByDirCopy = copy.deepcopy(tilesByDir)
+    seenCopy = copy.deepcopy(seen)
 
     trap = ()
 
@@ -83,8 +77,8 @@ def obstacleInFront(lines, i, j, dir, tilesByDir):
                 trap = (i, j - 1)
     
     foundLoop = False
-    if trap not in firstLine:
-        lines, foundLoop = processGuard(linesCopy, i, j, dir, True, tilesByDirCopy)
+    if trap != () and trap not in seen:
+        lines, foundLoop = processGuard(linesCopy, i, j, dir, True, tilesByDirCopy, seenCopy)
 
     if foundLoop and trap != ():
         total.add(trap)
@@ -93,13 +87,13 @@ def obstacleInFront(lines, i, j, dir, tilesByDir):
     
 
 if __name__ == "__main__":
-    lines = base.readFile("2024\\Day6\\test5")
+    lines = base.readFile("2024\\Day6\\input.txt")
 
     orig = base.findGuard(lines)
 
     tilesByDir = [[] for x in range(4)]
 
-    lines = processGuard(lines, orig[0], orig[1], 0, False, tilesByDir)
+    lines = processGuard(lines, orig[0], orig[1], 0, False, tilesByDir, set())
 
-    print(len(total))
     print(total)
+    print(len(total))
