9909e62c323b0a699074abe8f1657e757c996277
ClementCadieux <cadieuxclement@gmail.com>
2024-12-08 10:11:47 -0500

    getAntennaLocations

--- 2024/Day8/main1.py
+++ 2024/Day8/main1.py
@@ -6,3 +6,19 @@ def readFile(path):
     lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
 
     return lines
+
+def getAntennaLocations(lines):
+    antennaLocations = {}
+
+    for i in range(len(lines)):
+        line = lines[i]
+        for j in range(len(line)):
+            tile = line[j]
+
+            if tile != ".":
+                if tile not in antennaLocations:
+                    antennaLocations[tile] = []
+
+                antennaLocations[tile].append((i, j))
+    
+    return antennaLocations
