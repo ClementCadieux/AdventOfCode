97e2830ac188b7cf907b2e53ed1b4a2507794cdd
ClementCadieux <cadieuxclement@gmail.com>
2024-12-09 07:31:41 -0500

    genMap

--- 2024/Day9/main1.py
+++ 2024/Day9/main1.py
@@ -5,3 +5,17 @@ def readFile(path):
 
     return line
 
+def genMap(line):
+    lineMap = ""
+    for i in range(len(line)):
+        charToAdd = str(int(i / 2)) if i % 2 == 0 else "."
+
+        length = int(line[i])
+
+        for j in range(length):
+            lineMap += charToAdd
+
+    return lineMap
+
+if __name__ == "__main__":
+    print("")
