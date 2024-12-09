da79004d2621c5e4078ecee9b573c042f41ad529
ClementCadieux <cadieuxclement@gmail.com>
2024-12-09 07:53:31 -0500

    reset main2

--- 2024/Day9/main2.py
+++ 2024/Day9/main2.py
@@ -1,7 +1,2 @@
-def compact(lineMap, filesByLength, fileStartIndex):
-    for length in filesByLength:
-        filesByLength[length].sort()
-
-    left = 0
-
-    while left = 
+def compact(lineMap):
+    return None
