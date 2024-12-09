0025e8e3ab91bd44b19296476058d374ea6e75c8
ClementCadieux <cadieuxclement@gmail.com>
2024-12-09 08:01:02 -0500

    getLineInfo

--- 2024/Day9/main2.py
+++ 2024/Day9/main2.py
@@ -1,2 +1,22 @@
 def compact(lineMap):
     return None
+
+def genLineInfo(line):
+    emptyLengths = {}
+    fileMetaDatas = {}
+
+    currIndex = 0
+
+    for i in range(len(line)):
+        length = int(line[i])
+
+        charId = str(int(i / 2)) if i % 2 == 0 else "."
+
+        if charId == ".":
+            emptyLengths[str(currIndex)] = length
+        else:
+            fileMetaDatas[charId] = (length, currIndex)
+
+        currIndex += length
+    
+    return fileMetaDatas, emptyLengths
