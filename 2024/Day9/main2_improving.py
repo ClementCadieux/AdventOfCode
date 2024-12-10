9c67acdaa0295fed6d88310c8968ab57cc723d66
ClementCadieux <cadieuxclement@gmail.com>
2024-12-10 12:37:49 -0500

    compact

--- 2024/Day9/main2_improving.py
+++ 2024/Day9/main2_improving.py
@@ -13,7 +13,7 @@ def getLineInfo(line):
             continue
 
         if i % 2 == 0:
-            filesMetaData.append((length, int(i / 2), currLength))
+            filesMetaData.append([length, int(i / 2), currLength])
         else:
             while len(emptysByLength) <= length:
                 emptysByLength.append([])
@@ -25,11 +25,39 @@ def getLineInfo(line):
     
     return emptysByLength, filesMetaData
 
+def compact(emptysByLength, filesMetaData):
+    for i in range(len(filesMetaData) - 1, -1, -1):
+        file = filesMetaData[i]
+        fileLength = file[0]
+        fileIdx = file[2]
+
+        minIdx = fileIdx
+        emptysIdx = -1
+
+        for j in range(fileLength, len(emptysByLength)):
+            if len(emptysByLength[j]) > 0 and emptysByLength[j][0] < minIdx:
+                emptysIdx = j
+                minIdx = emptysByLength[j][0]
+        
+        if emptysIdx != -1:
+            file[2] = minIdx
+
+            newEmptyLength = emptysIdx - fileLength
+            newEmptyIndex = minIdx + fileLength
+
+            del emptysByLength[emptysIdx][0]
+            emptysByLength[newEmptyLength].append(newEmptyIndex)
+
+            emptysByLength[newEmptyLength].sort()
+        
+    return filesMetaData
+
 
 if __name__ == "__main__":
     line = base1.readFile("2024\\Day9\\test.txt")
 
     emptysByLength, filesMetaData = getLineInfo(line)
 
-    print(emptysByLength)
+    filesMetaData = compact(emptysByLength, filesMetaData)
+
     print(filesMetaData)
