6f3488514d26ce142cc1a3ee60631d9e4d219597
ClementCadieux <cadieuxclement@gmail.com>
2024-12-10 08:10:31 -0500

    big reset

--- 2024/Day10/main1.py
+++ 2024/Day10/main1.py
@@ -1,5 +1,3 @@
-import functools
-
 lines = []
 
 def readFile(path):
@@ -13,57 +11,9 @@ def readFile(path):
 
     return intLines
 
-def findZeroes(lines):
-    zeroSpots = []
 
-    for i in range(len(lines)):
-        for j in range(len(lines[i])):
-            if lines[i][j] == 0:
-                zeroSpots.append((i, j))
-
-    return zeroSpots
-
-@functools.lru_cache(maxsize=None)
-def hikingScore(i, j):
-    global lines
-    
-    val = lines[i][j]
-    lines[i][j] = -1
-
-    if val == 9:
-        return 1
-
-    up = -1 if i == 0 else lines[i - 1][j]
-    down = -1 if i == len(lines) - 1 else lines[i + 1][j]
-    left = -1 if j == 0 else lines[i][j - 1]
-    right = -1 if j == len(lines[i]) - 1 else lines[i][j + 1]
-
-    score = 0
-
-    if up == val + 1:
-        score += hikingScore(i - 1, j)
-
-    if down == val + 1:
-        score += hikingScore(i + 1, j)
-
-    if left == val + 1:
-        score += hikingScore(i, j - 1)
-
-    if right == val + 1:
-        score += hikingScore(i, j + 1)
-
-    lines[i][j] = val
-
-    return score
 
 if __name__ == "__main__":
     lines = readFile("2024\\Day10\\test.txt")
 
-    zeroSpots = findZeroes(lines)
-
-    total = 0
-
-    for spot in zeroSpots:
-        total += hikingScore(spot[0], spot[1])
-
-    print(total)
+    
