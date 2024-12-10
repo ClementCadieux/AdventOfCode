81335913333e9242504ecdd17d23bfd7022c27cf
ClementCadieux <cadieuxclement@gmail.com>
2024-12-10 07:55:21 -0500

    readFile

---
+++ 2024/Day10/main1.py
@@ -0,0 +1,15 @@
+def readFile(path):
+    file = open(path, "r")
+
+    lines = [line[:-1] if line[-1] == "\n" else line for line in file.readlines()]
+
+    splitLines = [list(line) for line in lines]
+
+    intLines = [[int(val) for val in line] for line in splitLines]
+
+    return intLines
+
+if __name__ == "__main__":
+    lines = readFile("2024\\Day10\\test2.txt")
+
+    
