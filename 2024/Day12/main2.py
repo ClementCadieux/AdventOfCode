ddca51ba2e7ea4e9b9bd89570729dafb7fe8be94
ClementCadieux <cadieuxclement@gmail.com>
2024-12-12 12:47:25 -0500

    looking good

--- 2024/Day12/main2.py
+++ 2024/Day12/main2.py
@@ -3,7 +3,40 @@ import main1 as base
 def calcRegionInfo(lines):
     sideGrid = sideByTiles(lines)
 
-    return None
+    regionInfo = []
+
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            regionNumber = lines[i][j]
+
+            while regionNumber >= len(regionInfo):
+                regionInfo.append([0, 0])
+            
+            regionInfo[regionNumber][0] += 1
+
+            sides = sideGrid[i][j]
+
+            upLeft = sides[0] or j == 0 or not sideGrid[i][j - 1][2]
+
+            if sides[2] and upLeft:
+                regionInfo[regionNumber][1] += 1
+            
+            downLeft = sides[0] or j == 0 or not sideGrid[i][j - 1][3]
+
+            if sides[3] and downLeft:
+                regionInfo[regionNumber][1] += 1
+
+            leftUp = sides[2] or i == 0 or not sideGrid[i - 1][j][0]
+
+            if sides[0] and leftUp:
+                regionInfo[regionNumber][1] += 1
+            
+            leftDown = sides[2] or i == 0 or not sideGrid[i - 1][j][1]
+
+            if sides[1] and leftDown:
+                regionInfo[regionNumber][1] += 1
+
+    return regionInfo
 
 def sideByTiles(lines):
     sideGrid = [[[] for tile in line] for line in lines]
@@ -16,28 +49,19 @@ def sideByTiles(lines):
             right = j == len(lines[i]) - 1 or lines[i][j + 1] != regionNum
             up = i == 0 or lines[i - 1][j] != regionNum
             down = i == len(lines) - 1 or lines[i + 1][j] != regionNum
-
-            sides = 0
-
-            if left:
-                sides += 1000
-            if right:
-                sides += 10
-            if up:
-                sides += 100
-            if down:
-                sides += 1
             
-            sideGrid[i][j] = sides
+            sideGrid[i][j] = [left, right, up, down]
     
     return sideGrid
 
 if __name__ == "__main__":
-    lines = base.readFile("2024\\Day12\\test.txt")
+    lines = base.readFile("2024\\Day12\\test2.txt")
 
     lines  = base.makeRegionsNumbers(lines)
 
     regionInfo = calcRegionInfo(lines)
+
+    print(regionInfo)
 
     cost = base.calcCost(regionInfo)
 
