b5b61bc017c572eb40f4f3715eb0606b41534be9
ClementCadieux <cadieuxclement@gmail.com>
2024-12-12 07:11:10 -0500

    Comments!

--- 2024/Day12/main1.py
+++ 2024/Day12/main1.py
@@ -6,3 +6,7 @@ def readFile(path):
     lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
 
     return lines
+
+# make each tile a tuple with two values:
+# (region tt belongs to, number of fences)
+
