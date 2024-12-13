07748307384179283870345d1438f9e4da716352
ClementCadieux <cadieuxclement@gmail.com>
2024-12-13 12:12:19 -0500

    YO ALGEBRA GOD

--- 2024/Day13/main2.py
+++ 2024/Day13/main2.py
@@ -12,23 +12,22 @@ def calcOptPrice(machine):
     prizeStep = prizeSub/aCoef
     bCoef /= aCoef
 
-    bPress = (prize[0] - (right[0] * prizeStep)) / (left[0] - (right[0] * bCoef))
-    aPress = prizeStep - (bCoef * bPress)
+    bPress = round((prize[0] - (right[0] * prizeStep)) / (left[0] - (right[0] * bCoef)))
+    aPress = round(prizeStep - (bCoef * bPress))
 
-    if bPress % 1 != 0 or aPress % 1 != 0:
+    if bPress * left[0] + aPress * right[0] != prize[0] or bPress * left[1] + aPress * right[1] != prize[1]:
         return 0
     
     return 3*aPress + bPress
 
 if __name__ == "__main__":
-    machines = base.readFile("2024\\Day13\\test.txt")
+    machines = base.readFile("2024\\Day13\\input.txt")
 
     total = 0
 
     for machine in machines:
         machine[2] = (machine[2][0] + (10 ** 13), machine[2][1] + (10 ** 13))
         price = calcOptPrice(machine)
-        print(price)
         total += price
     
     print(total)
