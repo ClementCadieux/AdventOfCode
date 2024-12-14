e29e2809220f3c6aa62eed129face4e9aa4ecc37
ClementCadieux <cadieuxclement@gmail.com>
2024-12-14 12:33:54 -0500

    Doneso

--- 2024/Day14/main2.py
+++ 2024/Day14/main2.py
@@ -1,4 +1,5 @@
 import main1 as base
+import time
 
 def moveRobots(robots, xBounds, yBounds):
     for robot in robots:
@@ -8,8 +9,25 @@ def moveRobots(robots, xBounds, yBounds):
         robot[0][0] %= xBounds
         robot[0][1] %= yBounds
 
+def writeRobots(robots):
+    grid = [['.' for i in range(101)] for j in range(103)]
+
+    for robot in robots:
+        grid[robot[0][1]][robot[0][0]] = "#"
+    
+    file = open("2024\\Day14\\currentState.txt", "w")
+
+    for line in grid:
+        file.write(str(line))
+        file.write("\n")
+
+    file.close()
+
 if __name__ == "__main__":
     robots = base.readFile("2024\\Day14\\input.txt")
 
     xBounds = 101
     yBounds = 103
+
+    base.moveRobots(6355, robots, xBounds, yBounds)
+    writeRobots(robots)     
