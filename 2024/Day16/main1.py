e629f540ca6573810922f5630974d4d80d8232bc
ClementCadieux <cadieuxclement@gmail.com>
2024-12-16 10:04:03 -0500

    not restart?

--- 2024/Day16/main1.py
+++ 2024/Day16/main1.py
@@ -9,3 +9,11 @@ lines = util.readFile(filePath)
 
 scores = [[0 for tile in line] for line in lines]
 
+if lines[2][-2] != "#":
+    util.scoreTile(scores, lines, 2, len(lines[1]) - 2, 1, 0)
+if lines[1][-3] != "#":
+    util.scoreTile(scores, lines, 1, len(lines[1]) - 2, 1, 3)
+
+score = scores[-2][1]
+
+print(score)
