4205cdcee807c9b901c607920967e424b09f1814
ClementCadieux <cadieuxclement@gmail.com>
2024-12-17 09:14:57 -0500

    readLine fixes

---
+++ 2024/Day17/main1.py
@@ -0,0 +1,10 @@
+import sys
+import util
+
+args = sys.argv
+
+filePath = "2024\\Day17\\test.txt" if len(args) == 1 else args[1]
+
+a, b, c, instructions = util.readFile(filePath)
+
+print(a, b, c, instructions)
--- 2024/Day17/util.py
+++ 2024/Day17/util.py
@@ -5,26 +5,24 @@ def readFile(path):
 
     lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
 
-    a = setRegister[lines[0]]
-    b = setRegister[lines[1]]
-    c = setRegister[lines[2]]
+    a = setRegister(lines[0])
+    b = setRegister(lines[1])
+    c = setRegister(lines[2])
 
     programLine = lines[4]
 
-    programInstructions = programLine.split(":")[1].trim()
+    programInstructions = programLine.split(":")[1].strip()
 
     program = programInstructions.split(",")
 
     program = [int(instruction) for instruction in program]
 
-    return [a, b, c, program]
-
-
+    return (a, b, c, program)
 
 def setRegister(line):
     splitLine = line.split(":")
 
-    numStr = splitLine[1].trim()
+    numStr = splitLine[1].strip()
 
     num = int(numStr)
 
