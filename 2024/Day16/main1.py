0777c6919fe2965eed0fb3a5b9f22ffac1f926a5
ClementCadieux <cadieuxclement@gmail.com>
2024-12-17 08:49:43 -0500

    p1 reset

--- 2024/Day16/main1.py
+++ 2024/Day16/main1.py
@@ -11,9 +11,9 @@ scores = [[0 for tile in line] for line in lines]
 seen = set()
 
 if lines[2][-2] != "#":
-    util.scoreTile(scores, lines, 2, len(lines[1]) - 2, 1, 0, seen)
+    util.scoreTile(scores, lines, 2, len(lines[1]) - 2, 1, 0)
 if lines[1][-3] != "#":
-    util.scoreTile(scores, lines, 1, len(lines[1]) - 2, 0, 1, seen)
+    util.scoreTile(scores, lines, 1, len(lines[1]) - 2, 0, 1)
 
 score = scores[-2][1]
 
--- 2024/Day16/util.py
+++ 2024/Day16/util.py
@@ -11,11 +11,11 @@ def readFile(path):
     return lines
 
 sys.setrecursionlimit(5000)
-def scoreTile(scores, maze, i, j, score, direction, seen):
+def scoreTile(scores, maze, i, j, score, direction):
     if scores[-2][1] != 0 and score > scores[-2][1]:
         return False
     
-    if (i, j) in seen or scores[i][j] != 0 and scores[i][j] < score:
+    if scores[i][j] != 0 and scores[i][j] < score:
         return False
 
     if maze[i][j] == "S":
@@ -30,8 +30,7 @@ def scoreTile(scores, maze, i, j, score, direction, seen):
         
         return True
     
-    # scores[i][j] = score
-    seen.add((i, j))
+    scores[i][j] = score
     
     upScore = score + 1 if direction == 0 else score + 1001 if direction in [1, 3] else -1
     downScore = score + 1 if direction == 2 else score + 1001 if direction in [1, 3] else -1
@@ -44,17 +43,17 @@ def scoreTile(scores, maze, i, j, score, direction, seen):
     left = False
 
     if maze[i][j - 1] != "#" and rightScore != -1:
-        right = scoreTile(scores, maze, i, j - 1, rightScore, 1, seen)
+        right = scoreTile(scores, maze, i, j - 1, rightScore, 1)
     if maze[i + 1][j] != "#" and upScore != -1:
-        up = scoreTile(scores, maze, i + 1, j, upScore, 0, seen)
+        up = scoreTile(scores, maze, i + 1, j, upScore, 0)
     if maze[i][j + 1] != "#" and leftScore != -1:
-        left = scoreTile(scores, maze, i, j + 1, leftScore, 3, seen)
+        left = scoreTile(scores, maze, i, j + 1, leftScore, 3)
     if maze[i - 1][j] != "#" and downScore != -1:
-        down = scoreTile(scores, maze, i - 1, j, downScore, 2, seen)
+        down = scoreTile(scores, maze, i - 1, j, downScore, 2)
 
-    if up or right or down or left:
-        scores[i][j] = scores[-2][1]
+    # if up or right or down or left:
+    #     scores[i][j] = scores[-2][1]
     
-    seen.remove((i, j))
+    # seen.remove((i, j))
 
     return up or right or down or left
