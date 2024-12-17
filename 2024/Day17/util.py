f059e93d1a485cc0918bc7e1b0e71c6a959bdade
ClementCadieux <cadieuxclement@gmail.com>
2024-12-17 09:10:38 -0500

    readFile

---
+++ 2024/Day17/util.py
@@ -0,0 +1,31 @@
+def readFile(path):
+    file = open(path, "r")
+
+    lines = file.readlines()
+
+    lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
+
+    a = setRegister[lines[0]]
+    b = setRegister[lines[1]]
+    c = setRegister[lines[2]]
+
+    programLine = lines[4]
+
+    programInstructions = programLine.split(":")[1].trim()
+
+    program = programInstructions.split(",")
+
+    program = [int(instruction) for instruction in program]
+
+    return [a, b, c, program]
+
+
+
+def setRegister(line):
+    splitLine = line.split(":")
+
+    numStr = splitLine[1].trim()
+
+    num = int(numStr)
+
+    return num
