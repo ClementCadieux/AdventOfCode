616facacdc20088d1d5310a4d62a90f6f9bf0dad
ClementCadieux <cadieuxclement@gmail.com>
2024-12-18 15:02:18 -0500

    timming p1

--- 2024/Day16/main1.py
+++ 2024/Day16/main1.py
@@ -1,9 +1,12 @@
 import sys
 import util
+import time
 
 args = sys.argv
 
 filePath = "2024\\Day16\\test.txt" if len(args) == 1 else args[1]
+
+start = time.time()
 
 lines = util.readFile(filePath)
 
@@ -24,3 +27,7 @@ util.dijkstra(scores, lines, len(lines) - 2, 1, unvisited, nonInfinityNodes, 1, 
 score = scores[1][-2]
 
 print(score)
+
+end = time.time()
+
+print(end - start)
