2b10687de023a951265759db4df5641c665e9b51
ClementCadieux <cadieuxclement@gmail.com>
2024-12-18 07:15:09 -0500

    genGrid

--- 2024/Day18/main1.py
+++ 2024/Day18/main1.py
@@ -4,5 +4,12 @@ import util
 args = sys.argv
 
 filePath = "2024\\Day18\\test.txt" if len(args) == 1 else args[1]
+bound = 7 if len(args) == 1 else int(args[2])
+sim = 12 if len(args) == 1 else int(args[3])
 
 coordsList = util.readFile(filePath)
+
+grid = util.genGrid(coordsList, bound, sim)
+
+for line in grid:
+    print(line)
--- 2024/Day18/util.py
+++ 2024/Day18/util.py
@@ -10,3 +10,12 @@ def readFile(filePath):
     intSplitLines = [[int(coord) for coord in line] for line in splitLines]
 
     return intSplitLines
+
+def genGrid(coords, bound, sim):
+    grid = [["." for j in range(bound)] for i in range(bound)]
+
+    for i in range(sim):
+        coord = coords[i]
+        grid[coord[1]][coord[0]] = "#"
+
+    return grid
