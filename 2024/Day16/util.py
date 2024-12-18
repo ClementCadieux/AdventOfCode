eeb49933c7040349196b5a7b358af636cdc676b9
ClementCadieux <cadieuxclement@gmail.com>
2024-12-18 14:33:01 -0500

    new dijkstra

--- 2024/Day16/util.py
+++ 2024/Day16/util.py
@@ -58,5 +58,53 @@ def scoreTile(scores, maze, i, j, score, direction):
 
     return up or right or down or left
 
+sys.setrecursionlimit(5000)
 def dijkstra(scoreGrid, grid, i, j, unvisited, nonInfinityNodes, direction):
-    return None
+    if (i, j) not in unvisited:
+        return
+
+    unvisited.remove((i, j))
+    nonInfinityNodes.pop(0)
+
+    if grid[i][j] == "#":
+        return
+
+    score = scoreGrid[i][j]
+
+    up = (i - 1, j)
+    down = (i + 1, j)
+    left = (i, j - 1)
+    right = (i, j + 1)
+
+    upPossible = i != 0 and grid[up[0]][up[1]] != "#" and up in unvisited and up not in nonInfinityNodes and direction in [0, 1, 3]
+    downPossible = i != len(grid) - 1 and grid[down[0]][down[1]] != "#" and down in unvisited and down not in nonInfinityNodes and direction in [1, 2, 3]
+    leftPossible = j != 0 and grid[left[0]][left[1]] != "#" and left in unvisited and left not in nonInfinityNodes and direction in [0, 2, 3]
+    rightPossible = j != len(grid[i]) - 1 and grid[right[0]][right[1]] != "#" and right in unvisited and right not in nonInfinityNodes and direction in [0, 1, 2]
+
+    if upPossible:
+        scoreGrid[up[0]][up[1]] = score + 1
+        if direction != 0:
+            scoreGrid[up[0]][up[1]] += 1000
+        nonInfinityNodes.append((up, 0))
+    if downPossible:
+        scoreGrid[down[0]][down[1]] = score + 1
+        if direction != 2:
+            scoreGrid[down[0]][down[1]] += 1000
+        nonInfinityNodes.append((down, 2))
+    if leftPossible:
+        scoreGrid[left[0]][left[1]] = score + 1
+        if direction != 3:
+            scoreGrid[left[0]][left[1]] += 1000
+        nonInfinityNodes.append((left, 3))
+    if rightPossible:
+        scoreGrid[right[0]][right[1]] = score + 1
+        if direction != 1:
+            scoreGrid[right[0]][right[1]] += 1000
+        nonInfinityNodes.append((right, 1))
+
+    if len(nonInfinityNodes) == 0:
+        return
+    
+    minNode, newDirection = nonInfinityNodes[0]
+
+    dijkstra(scoreGrid, grid, minNode[0], minNode[1], unvisited, nonInfinityNodes, newDirection)
