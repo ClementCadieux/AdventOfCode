aaaba3e6463b3a7733471844ec5156beb8a6cb9a
ClementCadieux <cadieuxclement@gmail.com>
2024-12-18 15:21:58 -0500

    start chnge

--- 2024/Day16/main2.py
+++ 2024/Day16/main2.py
@@ -2,27 +2,17 @@ import sys
 import util
 import time
 
-start = time.time()
-
 args = sys.argv
 
 filePath = "2024\\Day16\\test3.txt" if len(args) == 1 else args[1]
 
+start = time.time()
+
 lines = util.readFile(filePath)
 
-scores = [[0 for tile in line] for line in lines]
-
-
+scores = util.scoreTiles(lines)
 
 finalScore = scores[-2][1]
-total = 0
-
-for line in scores:
-    for tile in line:
-        if tile == finalScore:
-            total += 1
-
-print(total + 1)
 
 end = time.time()
 
