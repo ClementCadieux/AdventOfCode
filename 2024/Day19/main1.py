c2e1c78f517945c1fa94ceb554abf8098585b335
ClementCadieux <cadieuxclement@gmail.com>
2024-12-19 07:16:37 -0500

    possible logic

---
+++ 2024/Day19/main1.py
@@ -0,0 +1,24 @@
+import sys
+import util
+import time
+
+args = sys.argv
+
+filePath = "2024\\Day19\\test.txt" if len(args) == 1 else args[1]
+
+start = time.time()
+
+towelsByFirst, designs = util.readFile(filePath)
+
+count = 0
+
+for design in designs:
+    valid = util.possibleDesign(design, 0, towelsByFirst)
+    if valid:
+        count += 1
+
+print(count)
+
+end = time.time()
+
+print(end - start)
--- 2024/Day19/util.py
+++ 2024/Day19/util.py
@@ -3,7 +3,7 @@ def readFile(path):
 
     lines = file.readlines()
 
-    lines = [line[:-1] if line[:-1] == "\n" else line for line in lines]
+    lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
 
     towelsLine = lines[0]
 
@@ -27,3 +27,29 @@ def readFile(path):
         designs.append(lines[i])
 
     return towelsByFirst, designs
+
+def possibleDesign(design, index, towelsByFirst):
+    if index == len(design):
+        return True
+    
+    currChar = design[index]
+
+    lengthToEnd = len(design) - index
+
+    towels = [] if currChar not in towelsByFirst else towelsByFirst[currChar]
+
+    validDesign = False
+
+    for towel in towels:
+        if len(towel) <= lengthToEnd:
+            valid = True
+            for i in range(len(towel)):
+                if towel[i] != design[index + i]:
+                    valid = False
+                    break
+            if valid:
+                validDesign = possibleDesign(design, index + len(towel), towelsByFirst)
+        if validDesign:
+            break
+
+    return validDesign
