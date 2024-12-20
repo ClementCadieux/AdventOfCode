417a5323b0da8f8831c4ad98460c2d0471fc3bb7
ClementCadieux <cadieuxclement@gmail.com>
2024-12-20 09:40:11 -0500

    readFIle

---
+++ 2024/Day20/main1.py
@@ -0,0 +1,11 @@
+import sys
+import util
+import time
+
+args = sys.argv
+
+filePath = "2024\\Day20\\test.txt" if len(args) == 1 else args[1]
+
+start = time.time()
+
+grid = util.readFile(filePath)
---
+++ 2024/Day20/util.py
@@ -0,0 +1,10 @@
+def readFile(path):
+    file = open(path,"r")
+
+    lines = file.readlines()
+
+    lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
+
+    grid = [list(line) for line in lines]
+
+    return grid
