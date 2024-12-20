2ecf2dbaab0cbb200bb47e0ade61bd570a02bad8
ClementCadieux <cadieuxclement@gmail.com>
2024-12-20 11:55:26 -0500

    main2 base setup

---
+++ 2024/Day20/main2.py
@@ -0,0 +1,31 @@
+import sys
+import util
+import time
+
+args = sys.argv
+
+filePath = "2024\\Day20\\test.txt" if len(args) == 1 else args[1]
+minScore = 74 if len(args) == 1 else int(args[2])
+
+startTime = time.time()
+
+grid = util.readFile(filePath)
+
+start = util.findStart(grid)
+
+maxScore = util.scorePath(grid, start)
+
+shortcutsByScore = [0 for i in range(maxScore + 1)]
+
+util.findShortcuts(grid, shortcutsByScore)
+
+total = 0
+
+for i in range(minScore, len(shortcutsByScore)):
+    total += shortcutsByScore[i]
+
+print(total)
+
+endTime = time.time()
+
+print(endTime - startTime)
