08d813ca676b32c2df5afcc48cbc57c682f47c61
ClementCadieux <cadieuxclement@gmail.com>
2024-12-20 11:55:06 -0500

    timing part 1

--- 2024/Day20/main1.py
+++ 2024/Day20/main1.py
@@ -7,7 +7,7 @@ args = sys.argv
 filePath = "2024\\Day20\\test.txt" if len(args) == 1 else args[1]
 minScore = 50 if len(args) == 1 else int(args[2])
 
-start = time.time()
+startTime = time.time()
 
 grid = util.readFile(filePath)
 
@@ -25,3 +25,7 @@ for i in range(minScore, len(shortcutsByScore)):
     total += shortcutsByScore[i]
 
 print(total)
+
+endTime = time.time()
+
+print(endTime - startTime)
