afd345e114bd943588aab5141f1e24ccd1c33f99
ClementCadieux <cadieuxclement@gmail.com>
2024-12-21 11:43:58 -0500

    codeScore

--- 2024/Day21/util.py
+++ 2024/Day21/util.py
@@ -6,3 +6,8 @@ def readFile(path):
     lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
 
     return lines
+
+def codeScore(code, sequenceLength):
+    numCode = int(code[:-1])
+
+    return numCode*sequenceLength
