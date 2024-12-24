950488dad06eb8fc2e9e8b3006a9214cf3f3afad
ClementCadieux <cadieuxclement@gmail.com>
2024-12-24 13:28:54 -0500

    OMG

--- 2024/Day24/main2.py
+++ 2024/Day24/main2.py
@@ -10,7 +10,26 @@ startTime = time.time()
 
 nodes, nodeIndex = util.readFile(filePath)
 
-util.visualDisplay(nodes, nodeIndex)
+# comment in below line if you want a visual display of the system
+# util.visualDisplay(nodes, nodeIndex)
+
+xBin = util.getBinary(nodes, nodeIndex, "x")
+yBin = util.getBinary(nodes, nodeIndex, "y")
+
+x = int(xBin, 2)
+y = int(yBin, 2)
+
+expected = x + y
+
+util.processOps(nodes, nodeIndex)
+
+resStr = util.getBinary(nodes, nodeIndex, "z")
+
+res = int(resStr, 2)
+
+print(res)
+print(expected)
+print(expected - res)
 
 endTime = time.time()
 
