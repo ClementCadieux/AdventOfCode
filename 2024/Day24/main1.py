89a13745a79b4609b1e56f27b346d63f9f65ad93
ClementCadieux <cadieuxclement@gmail.com>
2024-12-24 08:43:35 -0500

    readFile

---
+++ 2024/Day24/main1.py
@@ -0,0 +1,12 @@
+import util
+import sys
+import time
+
+args = sys.argv
+
+filePath = "2024\\Day24\\test.txt" if len(args) == 1 else args[1]
+
+startTime = time.time()
+
+nodes = util.readFile(filePath)
+
---
+++ 2024/Day24/util.py
@@ -0,0 +1,52 @@
+def readFile(path):
+    file = open(path, "r")
+
+    lines = file.readlines()
+
+    lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
+
+    nodeIndex = {}
+    nodes = []
+
+    inWires = True
+
+    for line in lines:
+        if inWires:
+            if line == "":
+                inWires = False
+            else:
+                splitLine = line.split(":")
+
+                node = splitLine[0]
+                value = True if splitLine[1].strip() == "1" else False
+
+                nodeIndex[node] = len(nodes)
+                nodes.append(value)
+        else:
+            splitLine = line.split("->")
+
+            leftSplit = splitLine[0].split(" ")
+
+            targetNode = splitLine[1].strip()
+
+            opLeftNode = leftSplit[0]
+            opRightNode = leftSplit[2]
+
+            op = leftSplit[1]
+
+            if opLeftNode not in nodeIndex:
+                nodeIndex[opLeftNode] = len(nodes)
+                nodes.append(-1)
+
+            if opRightNode not in nodeIndex:
+                nodeIndex[opRightNode] = len(nodes)
+                nodes.append(-1)
+
+            if targetNode not in nodeIndex:
+                nodeIndex[targetNode] = len(nodes)
+                nodes.append(-1)
+
+            nodes[nodeIndex[targetNode]] = (nodeIndex[opLeftNode], op, nodeIndex[opRightNode])
+
+    return nodes
+
