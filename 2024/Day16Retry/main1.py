47b332ac53528193e7ea323ccea367487adf73c3
ClementCadieux <cadieuxclement@gmail.com>
2024-12-25 09:17:26 -0500

    pt 1 works

--- 2024/Day16Retry/main1.py
+++ 2024/Day16Retry/main1.py
@@ -1,11 +1,36 @@
 import sys
-import time
 import util
+import time
 
 args = sys.argv
 
-filePath = "2024\\Day16Retry\\test.txt" if len(args) == 1 else args[1]
+filePath = "2024\\Day16\\test.txt" if len(args) == 1 else args[1]
 
 startTime = time.time()
 
 grid = util.readFile(filePath)
+
+boundI = len(grid)
+boundJ = len(grid[0])
+
+scoreGrid = [[boundJ*boundI + 1 for y in range(boundJ)] for x in range(boundI)]
+unvisited = set()
+
+for i in range(boundI):
+    for j in range(boundJ):
+        unvisited.add((i, j))
+
+scoreGrid[boundI - 2][1] = 0
+
+nonInfinityNodes = []
+nonInfinityNodes.append((boundI - 2, 1))
+
+util.dijkstra(scoreGrid, grid, boundI - 2, 1, unvisited, nonInfinityNodes, 1, 0)
+
+score = scoreGrid[1][-2]
+
+print(score)
+
+endTime = time.time()
+
+print(endTime - startTime)
