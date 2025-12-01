5e897a39ef6692858981ca559436ac33742e5693
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-12-01 13:21:13 -0500

    extract readFile

--- 2025/Day1/main1.py
+++ 2025/Day1/main1.py
@@ -1,22 +1,5 @@
 import sys
-
-def readFile(path):
-    file = open(path, "r")
-
-    rotations = []
-
-    for line in file.readlines():
-        dir = line[0]
-        num = int(line[1:])
-
-        polarity = -1 if dir == 'L' else 1
-
-        val = num * polarity
-
-        rotations.append(val)
-    
-    return rotations
-
+from util import readFile
 
 if __name__ == "__main__":
     filePath = sys.argv[1]
