a64e5bde541ead5b70f44b1a0940d45ecfd3be6a
ClementCadieux <cadieuxclement@gmail.com>
2025-12-04 18:08:15 -0500

    readFile improvement

--- 2015/Day9/main1.py
+++ 2015/Day9/main1.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     filePath = "2015\\Day9\\test.txt" if len(sys.argv) < 2 else sys.argv[1]
 
-    lines = readFile(filePath)
+    distances = readFile(filePath)
 
-    
+    print(distances)
     
--- 2015/Day9/util.py
+++ 2015/Day9/util.py
@@ -7,4 +7,20 @@ def readFile(filePath):
 
     processSplitLines = [[line[0].split(" to "), int(line[1])] for line in splitLines]
 
-    return processSplitLines
+    resultDistance = {}
+
+    for line in processSplitLines:
+        city1 = line[0][0]
+        city2 = line[0][1]
+        distance = line[1]
+
+        city1Distances = {} if city1 not in resultDistance else resultDistance[city1]
+        city2Distances = {} if city2 not in resultDistance else resultDistance[city2]
+
+        city1Distances[city2] = distance
+        city2Distances[city1] = distance
+
+        resultDistance[city1] = city1Distances
+        resultDistance[city2] = city2Distances
+
+    return resultDistance
