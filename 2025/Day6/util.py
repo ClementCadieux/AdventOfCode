e4eb93c9d44e7652c3135ecbbaefd012d4c57fa0
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-12-06 08:30:14 -0500

    getProblems

--- 2025/Day6/util.py
+++ 2025/Day6/util.py
@@ -8,3 +8,23 @@ def readFile(filePath):
     outputLines = [[val for val in line if val] for line in splitLines]
 
     return outputLines
+
+def getProblems(lines):
+    problems = []
+
+    line1 = lines[0]
+    line2 = lines[1]
+    line3 = lines[2]
+
+    ops = lines[3]
+
+    for i in range(len(line1)):
+        val1 = int(line1[i])
+        val2 = int(line2[i])
+        val3 = int(line3[i])
+
+        op = ops[i]
+
+        problems.append([[val1, val2, val3], op])
+
+    return problems
