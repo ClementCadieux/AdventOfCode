197f80f12d95db51f4075f5a0442342777050169
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-12-06 09:16:12 -0500

    done

--- 2025/Day6/main1.py
+++ 2025/Day6/main1.py
@@ -1,6 +1,22 @@
 from util import readFile, getProblems
 import sys
 
+def computeProblem(problem):
+    op = problem[1]
+
+    solution = 0
+
+    if op == "+":
+        solution = sum(problem[0])
+    else:
+        nums = problem[0]
+        solution = 1
+
+        for num in nums:
+            solution *= num
+
+    return solution
+
 if __name__ == "__main__":
     filePath = "2025\\Day6\\test.txt" if len(sys.argv) < 2 else sys.argv[1]
 
@@ -11,18 +27,7 @@ if __name__ == "__main__":
     total = 0
 
     for problem in problems:
-        op = problem[1]
-
-        solution = 0
-
-        if op == "+":
-            solution = sum(problem[0])
-        else:
-            nums = problem[0]
-            solution = 1
-
-            for num in nums:
-                solution *= num
+        solution = computeProblem(problem)
 
         total += solution
 
--- 2025/Day6/main2.py
+++ 2025/Day6/main2.py
@@ -1,5 +1,6 @@
 from util import readFile2, getProblems2
 import sys
+from main1 import computeProblem
 
 if __name__ == "__main__":
     filePath = "2025\\Day6\\test.txt" if len(sys.argv) < 2 else sys.argv[1]
@@ -8,4 +9,11 @@ if __name__ == "__main__":
 
     problems = getProblems2(lines)
 
-    print(problems)
+    total = 0
+
+    for problem in problems:
+        solution = computeProblem(problem)
+
+        total += solution
+
+    print(total)
