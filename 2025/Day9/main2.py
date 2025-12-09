795efc7decedf5b9b5e8027687f540f25fa6884f
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-12-09 09:52:15 -0500

    getPointDirections

--- 2025/Day9/main2.py
+++ 2025/Day9/main2.py
@@ -1,27 +1,37 @@
 from util import readFile
 import sys
 
-def getMinMaxPoints(redTiles, maxLine, maxCol):
-    minMaxRedPointLines = [[-1, -1] for _ in range(maxLine + 1)]
-    minMaxRedPointCols = [[-1, -1] for _ in range(maxCol + 1)]
+def getDirections(redTiles):
+    pointDirections = [[False, False, False, False] for _ in redTiles]
 
-    for tile in redTiles:
-        line = tile[0]
-        col = tile[1]
+    for i in range(len(redTiles)):
+        currPoint = redTiles[i]
+        pointBefore = redTiles[i - 1]
+        pointAfter = redTiles[(i + 1) % len(redTiles)]
 
-        if minMaxRedPointCols[col][0] == -1 or minMaxRedPointCols[col][0] > line:
-            minMaxRedPointCols[col][0] = line
-        
-        if minMaxRedPointCols[col][1] == -1 or minMaxRedPointCols[col][1] < line:
-            minMaxRedPointCols[col][1] = line
+        if currPoint[0] == pointAfter[0]:
+            if currPoint[1] > pointAfter[1]:
+                pointDirections[i][3] = True
+            else:
+                pointDirections[i][1] = True
+        else:
+            if currPoint[0] > pointAfter[0]:
+                pointDirections[i][2] = True
+            else:
+                pointDirections[i][0] = True
 
-        if minMaxRedPointLines[line][0] == -1 or minMaxRedPointLines[line][0] > col:
-            minMaxRedPointLines[line][0] = col
-        
-        if minMaxRedPointLines[line][1] == -1 or minMaxRedPointLines[line][1] < col:
-            minMaxRedPointLines[line][1] = col
+        if currPoint[0] == pointBefore[0]:
+            if currPoint[1] > pointBefore[1]:
+                pointDirections[i][3] = True
+            else:
+                pointDirections[i][1] = True
+        else:
+            if currPoint[0] > pointBefore[0]:
+                pointDirections[i][2] = True
+            else:
+                pointDirections[i][0] = True
 
-    return minMaxRedPointLines, minMaxRedPointCols
+    return pointDirections
 
 if __name__ == "__main__":
     filePath = "2025\\Day9\\test.txt" if len(sys.argv) < 2 else sys.argv[1]
@@ -38,8 +48,6 @@ if __name__ == "__main__":
         if tile[1] > maxCol:
             maxCol = tile[1]
 
-    minMaxRedPointLines, minMaxRedPointCols = getMinMaxPoints(redTiles, maxLine, maxCol)
-
     maxArea = 0
 
     for i in range(len(redTiles) - 1):
@@ -48,15 +56,6 @@ if __name__ == "__main__":
             point2 = redTiles[j]
 
             valid = True
-
-            if point1[0] < minMaxRedPointCols[point2[1]][0] or point1[0] > minMaxRedPointCols[point2[1]][1]:
-                valid = False
-            elif point1[1] < minMaxRedPointLines[point2[0]][0] or point1[1] > minMaxRedPointLines[point2[0]][1]:
-                valid = False
-            elif point2[0] < minMaxRedPointCols[point1[1]][0] or point2[0] > minMaxRedPointCols[point1[1]][1]:
-                valid = False
-            elif point2[1] < minMaxRedPointLines[point1[0]][0] or point2[1] > minMaxRedPointLines[point1[0]][1]:
-                valid = False
 
             if valid:
                 xDiff = abs(point1[0] - point2[0]) + 1
