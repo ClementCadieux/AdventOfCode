21a7f586c03d552aa67582af46698e3e2a1adbe7
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-12-09 11:25:06 -0500

    getGrid

--- 2025/Day9/main2.py
+++ 2025/Day9/main2.py
@@ -1,8 +1,7 @@
 from util import readFile
 import sys
 
-def getDirections(redTiles):
-    pointDirections = [[False, False, False, False] for _ in redTiles]
+def getGrid(redTiles):
     grid = [[0 for _ in range(maxCol + 1)] for _ in range(maxLine + 1)]
 
     for i in range(len(redTiles)):
@@ -11,31 +10,27 @@ def getDirections(redTiles):
 
         if currPoint[0] == pointAfter[0]:
             if currPoint[1] > pointAfter[1]:
-                pointDirections[i][3] = True
                 x = currPoint[0]
                 for y in range(pointAfter[1], currPoint[1] + 1):
                     grid[x][y] = 1
             else:
-                pointDirections[i][1] = True
                 x = currPoint[0]
                 for y in range(currPoint[1], pointAfter[1] + 1):
                     grid[x][y] = 1
         else:
             if currPoint[0] > pointAfter[0]:
-                pointDirections[i][2] = True
                 y = currPoint[1]
                 for x in range(pointAfter[0], currPoint[0] + 1):
                     grid[x][y] = 1
             else:
-                pointDirections[i][0] = True
                 y = currPoint[1]
                 for x in range(currPoint[0], pointAfter[0] + 1):
                     grid[x][y] = 1
 
-    return pointDirections, grid
+    return grid
 
-def checkValid(grid, pointDirections, i, j, point1, point2):
-    
+def checkValid(grid, point1, point2):
+    return False
 
 if __name__ == "__main__":
     filePath = "2025\\Day9\\test.txt" if len(sys.argv) < 2 else sys.argv[1]
@@ -52,7 +47,7 @@ if __name__ == "__main__":
         if tile[1] > maxCol:
             maxCol = tile[1]
 
-    pointDirections, grid = getDirections(redTiles)
+    grid = getGrid(redTiles)
 
     maxArea = 0
 
@@ -60,16 +55,14 @@ if __name__ == "__main__":
         point1 = redTiles[i]
         for j in range(i + 1, len(redTiles)):
             point2 = redTiles[j]
+            
+            xDiff = abs(point1[0] - point2[0]) + 1
+            yDiff = abs(point1[1] - point2[1]) + 1
 
-            valid = checkValid(grid, pointDirections, i, j, point1, point2)
+            area = xDiff * yDiff
 
-            if valid:
-                xDiff = abs(point1[0] - point2[0]) + 1
-                yDiff = abs(point1[1] - point2[1]) + 1
-
-                area = xDiff * yDiff
-
-                if area > maxArea:
+            if area > maxArea:
+                if checkValid(grid, point1, point2):
                     maxArea = area
 
     print(maxArea)
