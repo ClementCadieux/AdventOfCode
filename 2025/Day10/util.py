70d8136ce6555599800b441e9457c7f05274e9b1
ClementCadieux <cadieuxclement@gmail.com>
2025-12-10 08:23:16 -0500

    readFile

---
+++ 2025/Day10/util.py
@@ -0,0 +1,14 @@
+def readFile(filePath):
+    file = open(filePath, 'r')
+
+    lines = [line.strip("\n") for line in file.readlines()]
+
+    splitLines = [line.split(" ") for line in lines]
+
+    sepLines = [[line[0], line[1:-1], line[-1]] for line in splitLines]
+
+    processedLines = [[line[0].strip("[]"), [val.strip("()").split(",") for val in line[1]], line[2].strip("{}").split(",")] for line in sepLines]
+
+    intMiddleLines = [[list(line[0]), [[int(val) for val in button] for button in line[1]], [int(val) for val in line[2]]] for line in processedLines]
+
+    return intMiddleLines
