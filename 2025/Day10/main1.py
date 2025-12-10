e47c94a108c8e37df448cd69648c497b3dc18d75
ClementCadieux <cadieuxclement@gmail.com>
2025-12-10 09:30:36 -0500

    recursion attempt: stack overflow on real input

--- 2025/Day10/main1.py
+++ 2025/Day10/main1.py
@@ -1,4 +1,4 @@
-from util import readFile
+from util import readFile, processMachineState
 import sys
 
 if __name__ == "__main__":
@@ -6,4 +6,13 @@ if __name__ == "__main__":
 
     lines = readFile(filePath)
 
-    
+    total = 0
+
+    for machine in lines:
+        print(machine)
+        cache = {}
+        currState = "".join(["." for _ in range(len(machine[0]))])
+
+        total += processMachineState(machine, cache, currState, -1)
+
+    print(total)
--- 2025/Day10/util.py
+++ 2025/Day10/util.py
@@ -1,3 +1,5 @@
+import sys
+
 def readFile(filePath):
     file = open(filePath, 'r')
 
@@ -9,6 +11,40 @@ def readFile(filePath):
 
     processedLines = [[line[0].strip("[]"), [val.strip("()").split(",") for val in line[1]], line[2].strip("{}").split(",")] for line in sepLines]
 
-    intMiddleLines = [[list(line[0]), [[int(val) for val in button] for button in line[1]], [int(val) for val in line[2]]] for line in processedLines]
+    intMiddleLines = [[line[0], [[int(val) for val in button] for button in line[1]], [int(val) for val in line[2]]] for line in processedLines]
 
     return intMiddleLines
+
+def processMachineState(machine, cache, currState, lastButton):
+    if currState == machine[0]:
+        return 0
+    
+    if "#" not in currState and lastButton != -1:
+        return sys.maxsize
+
+    if currState in cache:
+        return cache[currState]
+    
+    cache[currState] = sys.maxsize
+
+    for i in range(len(machine[1])):
+        if i != lastButton:
+            button = machine[1][i]
+            newState = processButton(currState, button)
+
+            iterationsFromHere = processMachineState(machine, cache, newState, i)  + 1
+
+            if iterationsFromHere < cache[currState]:
+                cache[currState] = min  
+
+    return cache[currState]
+
+def processButton(currState, button):
+    for light in button:
+        newChar = "."
+        if currState[light] == ".":
+            newChar = "#"
+
+        currState = currState[:light] + newChar + currState[light + 1:]
+
+    return currState  
