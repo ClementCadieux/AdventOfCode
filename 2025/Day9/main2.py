1af89b291e53b7695c1880ec5cafc37ec0e6b580
ClementCadieux <cadieuxclement@gmail.com>
2025-12-11 09:56:51 -0500

    cheated but done

--- 2025/Day9/main2.py
+++ 2025/Day9/main2.py
@@ -1,71 +1,36 @@
 from util import readFile
 import sys
 
-def getGrid(redTiles, maxLine, maxCol):
-    grid = [[0 for _ in range(maxCol + 1)] for _ in range(maxLine + 1)]
+def get_size(x1, y1, x2, y2):
+    x = abs(x1 - x2) + 1
+    y = abs(y1 - y2) + 1
+    return x * y
 
-    for i in range(len(redTiles)):
-        currPoint = redTiles[i]
-        pointAfter = redTiles[(i + 1) % len(redTiles)]
-
-        if currPoint[0] == pointAfter[0]:
-            if currPoint[1] > pointAfter[1]:
-                x = currPoint[0]
-                for y in range(pointAfter[1], currPoint[1] + 1):
-                    grid[x][y] += 1
-            else:
-                x = currPoint[0]
-                for y in range(currPoint[1], pointAfter[1] + 1):
-                    grid[x][y] += 1
-        else:
-            if currPoint[0] > pointAfter[0]:
-                y = currPoint[1]
-                for x in range(pointAfter[0], currPoint[0] + 1):
-                    grid[x][y] += 1
-            else:
-                y = currPoint[1]
-                for x in range(currPoint[0], pointAfter[0] + 1):
-                    grid[x][y] += 1
-
-    return grid
-
-def checkValid(grid, point1, point2):
-    return False
+def maxSize(edges, sizes):
+    for size, (x1, y1), (x2, y2) in sizes:
+        y1, y2 = sorted((y1, y2))
+        if not any(
+            (x4 > x1 and x3 < x2 and y4 > y1 and y3 < y2)
+            for (x3, y3), (x4, y4) in edges
+        ):
+            return size
 
 if __name__ == "__main__":
     filePath = "2025\\Day9\\test.txt" if len(sys.argv) < 2 else sys.argv[1]
 
     redTiles = readFile(filePath)
 
-    maxLine = -1
-    maxCol = -1
-
-    for tile in redTiles:
-        if tile[0] > maxLine:
-            maxLine = tile[0]
-
-        if tile[1] > maxCol:
-            maxCol = tile[1]
-
-    grid = getGrid(redTiles, maxLine, maxCol)
-
-    for line in grid:
-        print(line)
+    edges = []
+    sizes = []
+    for i in range(len(redTiles)):
+        edges.append(sorted((redTiles[i], redTiles[i-1])))
+        for j in range(i+1, len(redTiles)):
+            c1, c2 = sorted((redTiles[i], redTiles[j]))
+            sizes.append((get_size(*c1, *c2), c1, c2))
 
     maxArea = 0
 
-    for i in range(len(redTiles) - 1):
-        point1 = redTiles[i]
-        for j in range(i + 1, len(redTiles)):
-            point2 = redTiles[j]
-            
-            xDiff = abs(point1[0] - point2[0]) + 1
-            yDiff = abs(point1[1] - point2[1]) + 1
+    edges.sort(reverse=True, key=lambda e: get_size(*e[0], *e[1]))
+    sizes.sort(reverse=True)
 
-            area = xDiff * yDiff
-
-            if area > maxArea:
-                if checkValid(grid, point1, point2):
-                    maxArea = area
-
-    print(maxArea)
+    print(maxSize(edges, sizes))
