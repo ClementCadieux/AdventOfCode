773f29796b2809011dae3f462867eccbfb0986e9
ClementCadieux <cadieuxclement@gmail.com>
2025-12-11 07:32:06 -0500

    part 1 done

---
+++ 2025/Day11/util.py
@@ -0,0 +1,26 @@
+def readFile(filePath):
+    file = open(filePath, 'r')
+
+    lines = [line.strip("\n") for line in file.readlines()]
+
+    splitLines = [line.split(":") for line in lines]
+
+    splitLinesTarget = [[line[0], line[1].strip().split(" ")] for line in splitLines]
+
+    circuitDict = {}
+
+    for line in splitLinesTarget:
+        circuitDict[line[0]] = line[1]
+
+    return circuitDict
+
+def pathsToOut(circuit, cache, machine):
+    if machine not in cache:
+        cache[machine] = 0
+
+        outputs = circuit[machine]
+
+        for output in outputs:
+            cache[machine] += pathsToOut(circuit, cache, output)
+    
+    return cache[machine]
